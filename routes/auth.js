a108d679c97fb15e0ae027cd6e574181f866dc9d
Rajeswaran <rajeswaran149@gmail.com>
2024-06-01 11:32:01 +0530

    update

--- routes/auth.js
+++ routes/auth.js
@@ -9,6 +9,7 @@ router.post('/signup' ,async (req,res) => {
     try{
        const hashedPassword = await bcrypt.hash(req.body.password , 10);
        const user = new User ({
+           username : req.body.username,
            email : req.body.email,
            password : hashedPassword
        })
